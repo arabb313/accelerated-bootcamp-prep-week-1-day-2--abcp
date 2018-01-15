aa7647a7a5ba7864de50ac157b0a81673ef40e7d
Ari Rabinowitz <arabinowitz94@gmail.com>
2018-01-15 05:06:36 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,26 +1,35 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
-}
+return [element,...array]
+  }
+
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+
+  array.unshift (element)
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
-
+return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
 
+array.push(element)
 }
 
 function accessElementInArray(array, index) {
-
+array.shift(element)
+return array
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+array.pop(element)
+return array
 }
+
 
 function removeElementFromBeginningOfArray(array) {
 }
@@ -30,5 +39,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-
+return array.slice(0, array.length - 1)
 }
